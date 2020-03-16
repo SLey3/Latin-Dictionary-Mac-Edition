dffdf02934ad222465a0a223026cd3863f879a7e
Sergio Ley-Languren <sergioley-languren@Sergios-Air.hsd1.ca.comcast.net>
2020-03-16 08:44:09 -0700

    Logo and windows.py update

--- windows.py
+++ windows.py
@@ -1,15 +1,22 @@
 ### Imports
 import tkinter as tk
 from tkinter import *
+from PIL import Image
 from pathlib import Path
 
-##Extra Code
-home = print(Path.home())
+### Extra Code
+picturePath1 = str(Path.home())+'/Latin_app/Pictures/Logo.jpeg'
+picturePath2 = str(Path.home())+'/Latin_app/Pictures/Icon.png'
 ### Application screen code
 window = tk.Tk()
 
 
-## Window Title
+## Window Title and Icon
+# Window Icon
+img = Image.open(r"Logo.jpg")
+img.save(r"Icon.png")
+#window.iconphoto(False, tk.PhotoImage(file=picturePath2))
+# Window Title
 window.title("Latin Unit 1 Dictionary - Based on Orion Academy Latin I , Unit 1")
 window.geometry('1110x950')
 
