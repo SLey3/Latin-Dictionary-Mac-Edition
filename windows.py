53acb68c895e7b839a574b4da55003378fe241a1
Sergio Ley-Languren <sergioley-languren@Sergios-Air.hsd1.ca.comcast.net>
2020-03-17 11:07:54 -0700

    Temp

--- windows.py
+++ windows.py
@@ -1,28 +1,27 @@
 ### Imports
 import tkinter as tk
 from tkinter import *
-from PIL import Image
 from pathlib import Path
-
 ### Extra Code
 picturePath1 = str(Path.home())+'/Latin_app/Pictures/Logo.jpeg'
 picturePath2 = str(Path.home())+'/Latin_app/Pictures/Icon.png'
-picturePath3 = str(Path.home())+'/Latin_app/Pictures/Icon.ico'
+picturePath3 = str(Path.home())+'/Latin_app/Pictures/Logo.ico'
 ### Application screen code
-window = tk.Tk()
+root = tk.Tk()
 
 
-## Window Title and Icon
+## Window Title, Icon, and Display
+# Window Display
+
 # Window Icon
-img = Image.open(picturePath1)
-img.save("Icon.ico")
-window.iconbitmap(picturePath3)
-# Task Bar Icon
+root.wm_iconbitmap('Icon.ico')
 
-window.iconphoto('wm', tk.PhotoImage(file=picturePath2))
+# Task Bar Icon
+root.iconphoto('wm', tk.PhotoImage(file=picturePath2))
+
 # Window Title
-window.title("Latin Unit 1 Dictionary - Based on Orion Academy Latin I , Unit 1")
-window.geometry('1110x950')
+root.title("Latin Unit 1 Dictionary - Based on Orion Academy Latin I , Unit 1")
+root.geometry('1110x950')
 
 ## Window Main Loop
-window.mainloop()
+root.mainloop()
