545b8ffc054a49597a4164afbe49eab5afe283ba
Sergio Ley-Languren <sergioley-languren@Sergios-Air.hsd1.ca.comcast.net>
2020-05-02 10:58:20 -0700

    for class

--- Application.py
+++ Application.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from pathlib import Path
 import tkinter as tk
+import csv
 
 # Paths
 picturePath1 = str(Path.home()) + '/Latin_app/Pictures/Logo.png'
@@ -20,11 +21,27 @@ def clearEntry():
     lbl_entry.delete(0, END)
     newTemplate = " "
     lbl_entry.insert(END, newTemplate)
-    return print("Text deleted")
+    return print("Text deleted", flush=True)
 
-    
-    
+def clearBox():
+    word_box.delete(0, END)
+    newTemplate = " "
+    word_box.insert(END, newTemplate)
+    return print("Box cleared", flush=True)
 
+# Csv file reader
+with open('LatinWordRef.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} and {row[1]} and {row[2]}')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+       
         
        
 # Label#1
@@ -33,6 +50,7 @@ title_lbl.grid(row=0, column=0)
 
 # Label#2
 global lbl_entry
+global word_box
 lbl_text = StringVar()
 text_lbl = Label(app, text="Search word:", font=('Times', 14), pady=20)
 text_lbl.grid(row=1, column=0, sticky=W)
@@ -46,9 +64,10 @@ scrollbar.grid(row=3, column=3)
 word_box.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=word_box.yview)
 
-# clear Button
-clear_btn = Button(app, text='Clear', command=clearEntry)
-clear_btn.grid(row=1, column=1, pady=20)
-
+# clear Buttons
+clear_btn1 = Button(app, text='Clear Entry', command=clearEntry)
+clear_btn1.grid(row=1, column=1, pady=20)
+clear_btn2 = Button(app, text='Clear box', command=clearBox)
+clear_btn2.grid(row=2, column=1, pady=20)
 # Program Runner
 app.mainloop()
