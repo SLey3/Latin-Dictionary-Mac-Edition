8e74b6c5ae97d69f60aac79bedc70ce5deec2754
Marisol Languren Sanchez <Langruen@Air-de-Marisol.hsd1.ca.comcast.net>
2020-05-02 12:05:37 -0700

    Progress#1 for Beta

--- Application.py
+++ Application.py
@@ -2,7 +2,7 @@ from tkinter import *
 from pathlib import Path
 import tkinter as tk
 import csv
-
+from LatinRef import Initial
 # Paths
 picturePath1 = str(Path.home()) + '/Latin_app/Pictures/Logo.png'
 
@@ -33,16 +33,18 @@ def clearBox():
 with open('LatinWordRef.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    ref = {}
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
-            line_count += 1
         else:
-            print(f'\t{row[0]} and {row[1]} and {row[2]}')
-            line_count += 1
+            print('\t'+ " and ".join(row[i] for i in range(len(row)) if row[i] != ""))
+            word = Initial(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+            ref[row[0]] = word
+        line_count += 1
     print(f'Processed {line_count} lines.')
-       
         
+ #TODO  responce = input(instructions) word = ref.get(responce, None) if not word: print(error message)      
        
 # Label#1
 title_lbl = Label(app, text="Latin Dictionary for Latin I" ,font=('bold', 35))
@@ -59,6 +61,7 @@ lbl_entry.grid(row=1, column=0)
 # Word list
 word_box = Listbox(app, height=25, width=115 , border=2)
 word_box.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+word_box.insert(0, "Salve = hello")
 scrollbar = Scrollbar(app)
 scrollbar.grid(row=3, column=3)
 word_box.configure(yscrollcommand=scrollbar.set)
